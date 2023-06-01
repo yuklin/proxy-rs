737457bad7fec5d033b5fe6fd66c0357f4b0f595
yuklin <linguanyu2018@gmail.com>
2023-06-01 10:18:32 +0800

    add table ss proto

--- proxy/src/bin/tls_proxy.rs
+++ proxy/src/bin/tls_proxy.rs
@@ -11,6 +11,7 @@ use std::{
 use argh::FromArgs;
 use bstr::{BStr, BString, ByteSlice, Bytes, Split, B};
 use env_logger;
+use format_bytes::format_bytes;
 use log::{debug, error, info, trace, warn};
 use rustls::{
     server::{Acceptor, ServerConfig},
@@ -26,6 +27,8 @@ use tokio::{
     time::timeout,
 };
 use tokio_rustls::{TlsAcceptor, TlsStream};
+
+use proxy::cipher::Table;
 
 const OVERFLOWSIZE: usize = 1024 * 1024 * 50;
 
@@ -47,6 +50,14 @@ struct Opt {
     /// fingerprints
     #[argh(option, short = 'f')]
     fp: Option<String>,
+
+    /// redirect
+    #[argh(option, short = 'r')]
+    redir: Option<String>,
+
+    /// passwd
+    #[argh(option, short = 'w')]
+    passwd: Option<String>,
 }
 
 fn load_cert(filepath: String) -> Vec<rustls::Certificate> {
@@ -181,6 +192,32 @@ fn parse_proxy_header(header_buff: &BString) -> ProxyProtocol {
     //    }
 }
 
+async fn r2w(r: &OwnedReadHalf, w: &OwnedWriteHalf, flag: &str) {
+    loop {
+        r.readable().await.unwrap();
+        let mut buff = [0; 2048];
+        let n = match r.try_read(&mut buff) {
+            Ok(n) => n,
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::WouldBlock => {
+                    debug!("{} read would block", &flag);
+                    continue;
+                }
+                _ => {
+                    debug!("{} read error: {:?}", &flag, &e);
+                    break;
+                }
+            },
+        };
+        debug!("{} read {} bytes, lets write", &flag, &n);
+        if n == 0 {
+            break;
+        }
+        w.writable().await.unwrap();
+        w.try_write(&buff[0..n]).unwrap();
+    }
+}
+
 #[tokio::main(worker_threads = 8)]
 async fn main() {
     env_logger::init();
@@ -197,6 +234,15 @@ async fn main() {
 
     let fp = opt.fp.unwrap_or_else(|| String::from("")).clone();
 
+    let redir = opt
+        .redir
+        .unwrap_or_else(|| String::from("127.0.0.1:5001"))
+        .clone();
+    let passwd = opt
+        .passwd
+        .unwrap_or_else(|| String::from("hehe123"))
+        .clone();
+
     let Some((raw_suites, exts)) = fp.split_once(",") else {
         panic!("fp format error")
     };
@@ -205,27 +251,30 @@ async fn main() {
 
     let acceptor = TlsAcceptor::from(Arc::new(server_config));
     let listener = TcpListener::bind(format!("0.0.0.0:{port}")).await.unwrap();
+
+    let mut ss_cipher = Table {
+        key: passwd,
+        ..Default::default()
+    };
+
+    ss_cipher.setup();
+
+    let mut ss_cipher = Arc::new(ss_cipher);
+
     loop {
         let (mut stream, addr) = listener.accept().await.unwrap();
         let raw_stream = stream.into_std().unwrap();
         let acceptor = acceptor.clone();
         let raw_suites = raw_suites.clone();
         let exts = exts.clone();
+        let ss_cipher = ss_cipher.clone();
+        let redir = redir.clone();
         tokio::spawn(async move {
             let mock_stream = raw_stream.try_clone().unwrap();
             let mut proxy_stream = TcpStream::from_std(raw_stream).unwrap();
 
             let (mut rr, rw) = proxy_stream.split();
 
-            //let (mut r, mut w) = stream.split();
-            //todo 解析协议头
-
-            //let mut proxy_header_buf = [0u8; 1024];
-            //let n = rr.read(&mut proxy_header_buf).await.unwrap();
-
-            //let mut proxy_protocol_header = Vec::new();
-            //let n = rr.read_to_end(&mut proxy_protocol_header).await.unwrap();
-            //
             let mut proxy_protocol_header = read_proxy_header(&rr).await;
             let proxy_protocol = parse_proxy_header(&proxy_protocol_header);
 
@@ -246,32 +295,6 @@ async fn main() {
 
                 let (mut r, mut w) = split(tls_stream);
 
-                //let mut target_header_buf = Vec::new();
-                //loop {
-                //    // wtf vec就不行, 也不报错
-                //    let mut buff = [0; 1024];
-                //    let n = match r.read(&mut buff).await {
-                //        Ok(n) => n,
-                //        Err(_) => break,
-                //    };
-                //    println!("{}", n);
-                //    target_header_buf.extend_from_slice(&buff[..n]);
-                //    if n == 0 || buff.len() > OVERFLOWSIZE || n < 1024 {
-                //        break;
-                //    }
-                //}
-                //dbg!(&target_header_buf);
-
-                ////let mut target_header_buf = [0u8; 1024];
-                ////let n = &r.read(&mut target_header_buf).await.unwrap();
-
-                ////let target_header_buf = &target_header_buf[..n].into();
-
-                //println!(
-                //    "https 协议 header & body\r\n-------------------------------\r\n{}",
-                //    std::str::from_utf8(target_header_buf.as_slice()).unwrap()
-                //);
-
                 let mut root_cert_store = RootCertStore::empty();
                 root_cert_store.add_server_trust_anchors(
                     webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
@@ -291,31 +314,86 @@ async fn main() {
                 let target = proxy_protocol.target;
                 dbg!(&target);
                 let mut host = String::new();
+                let mut port = 443;
 
                 match target.find(':') {
                     None => host = target.clone(),
-                    Some(n) => host = target[..n].to_string(),
+                    Some(n) => {
+                        host = target[..n].to_string();
+                        port = target[n + 1..].parse::<u16>().unwrap();
+                    }
                 }
-                dbg!(&host);
 
                 let mut suites = Vec::new();
                 for i in raw_suites.split('-') {
                     suites.push(CipherSuite::from(i.parse::<u16>().unwrap()));
                 }
                 client_config.custom_cipher_suites = Some(suites);
-                client_config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+                // we can not use alpn
+                //client_config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+                client_config.alpn_protocols = Vec::new();
 
                 client_config.custom_extensions = Some(exts.to_string());
 
                 let conn = tokio_rustls::TlsConnector::from(Arc::new(client_config));
-                let mut target_stream = tokio::net::TcpStream::connect(target);
-                let target_stream = timeout(Duration::from_secs(3), target_stream)
+
+                // target now is a ss proxy
+                //let mut target_stream = tokio::net::TcpStream::connect(target);
+                let mut target_stream = tokio::net::TcpStream::connect(redir);
+
+                // i can not modify client hello data, so use this pair to get data change data
+                // write data
+                let (hook_stream_in, mut hook_stream_out) = tokio::net::UnixStream::pair().unwrap();
+                let mut target_tcp_stream = timeout(Duration::from_secs(3), target_stream)
                     .await
                     .unwrap()
                     .unwrap();
 
+                let mut ss_header: Vec<u8> = Vec::new();
+                ss_header.push(0x03);
+                ss_header.push(host.len() as u8);
+                ss_header.append(host.clone().into_bytes().as_mut());
+                ss_header.append(port.to_be_bytes().to_vec().as_mut());
+
+                let ss_header = ss_cipher.encrypt(&mut ss_header);
+
+                let de_cipher = ss_cipher.clone();
+
+                target_tcp_stream.write(ss_header.as_slice()).await.unwrap();
+
+                let (mut target_tcp_read, mut target_tcp_write) = target_tcp_stream.into_split();
+                let (mut hook_read, mut hook_write) = hook_stream_out.into_split();
+
+                tokio::spawn(async move {
+                    loop {
+                        let mut buff: Vec<u8> = Vec::with_capacity(1024);
+                        hook_read.readable().await.unwrap();
+                        let n = hook_read.read_buf(&mut buff).await.unwrap();
+                        if n == 0 {
+                            break;
+                        }
+                        let buff = ss_cipher.encrypt(&mut buff);
+                        target_tcp_write.writable().await.unwrap();
+                        target_tcp_write.write(buff.as_slice()).await.unwrap();
+                    }
+                });
+
+                tokio::spawn(async move {
+                    loop {
+                        let mut buff: Vec<u8> = Vec::with_capacity(1024);
+                        target_tcp_read.readable().await.unwrap();
+                        let n = target_tcp_read.read_buf(&mut buff).await.unwrap();
+                        if n == 0 {
+                            break;
+                        }
+                        let buff = de_cipher.decrypt(&mut buff);
+                        hook_write.writable().await.unwrap();
+                        hook_write.write(buff.as_slice()).await.unwrap();
+                    }
+                });
+
                 let target_stream = conn
-                    .connect(host.as_str().try_into().unwrap(), target_stream)
+                    .connect(host.as_str().try_into().unwrap(), hook_stream_in)
                     .await
                     .unwrap();
 
@@ -323,7 +401,6 @@ async fn main() {
 
                 tokio::spawn(async move {
                     let buf = io::copy(&mut r, &mut tw).await.unwrap();
-                    dbg!(buf);
                 });
                 tokio::spawn(async move {
                     io::copy(&mut tr, &mut w).await.unwrap();
